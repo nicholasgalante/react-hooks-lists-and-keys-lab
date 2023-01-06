c870569d1045c38dac47b4de4762868de02f7d07
Nicholas Galante <nicholasgalante94@gmail.com>
2023-01-06 10:53:39 -0500

    Udpate lists

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+
+  const techArray = technologies.map(element=>{
+    return <span key={element}>{element}</span>
+  })
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {techArray}
       </div>
     </div>
   );
